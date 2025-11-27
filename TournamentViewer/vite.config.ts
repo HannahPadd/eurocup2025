5dc99be43c8b6b9c77677e1cebe9b1be32db39ee
Benjamin Vitters <vittersdesign@hotmail.com>
2025-11-27 20:46:58 +0100

    whoopsy

--- TournamentViewer/vite.config.ts
+++ TournamentViewer/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    origin: "http://0.0.0.0:5173"
+    origin: "http://0.0.0.0:5173",
+    allowedHosts: ["manager.itgeurocup.com"],
   },
 });
