40c21404d3c9b274d603406ccb622f5c15b0c0c8
Benjamin Vitters <vittersdesign@hotmail.com>
2025-12-02 21:14:08 +0100

    Enhance magic to SignUp

--- TournamentViewer/src/pages/SignUp.tsx
+++ TournamentViewer/src/pages/SignUp.tsx
@@ -5,7 +5,9 @@ import RegisterForm from "../components/manage/players/RegisterForm";
 export default function SignUpPage() {
     return (
         <div>
-            <h1 className="text-3xl">Sign up!</h1>
+            <div className="w-full max-w-sm mx-auto space-y-4">
+                <h1 className="text-3xl  mb-2 mt-4">Sign up!</h1>
+            </div>
             <div>
                 <RegisterForm/>
             </div>
