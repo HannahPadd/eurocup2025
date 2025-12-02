9087f0bae13887c9fd98bc7edda73d2a58b02a77
Hannah Lynn Lindrob <hannahlindrob@outlook.com>
2025-12-02 17:19:21 +0100

    Added basic user creation

---
+++ TournamentViewer/src/components/manage/players/RegisterForm.tsx
@@ -0,0 +1,51 @@
+import { createPlayer } from '../../../services/player/player.api';
+import { useState } from 'react';
+
+
+
+export default function RegisterForm() {
+        interface SignUpFormState {
+        name: string;
+        email: string;
+        password: string;
+    };
+
+    const [formData, setFormData] = useState<SignUpFormState> ({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData(prevData => ({...prevData, [name]: value}))
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        createPlayer(formData);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='name'>Player Name:</label>
+                <input type="text" name="name" onChange={handleChange}value={formData.name} required maxLength={20}/>
+            </div>
+
+            <div>
+                <label htmlFor='email'>Email:</label>
+                <input type="text" name="email" onChange={handleChange}value={formData.email} required maxLength={50}/>
+            </div>
+
+            <div>
+                <label htmlFor='password'>Password:</label>
+                <input type="password" name="password" onChange={handleChange}value={formData.password} required maxLength={30}/>
+            </div>
+
+            <div>
+                <button className="bg-lighter text-white p-2 rounded-lg">Sign up!</button>
+            </div>
+        </form>
+    )
+}
--- TournamentViewer/src/services/player/player.api.ts
+++ TournamentViewer/src/services/player/player.api.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 import { Player } from "../../models/Player";
+import { createPlayerRequest } from "../../models/requests/player-requests";
 
-export async function createPlayer() {
-
+export async function createPlayer(request: createPlayerRequest): Promise<Player> {
+    try {
+        const response = await axios.post<Player>("players/", request);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating player:", error);
+        throw new Error("Unable to create player.");
+    }
 }
 
 export async function getPlayer(playerId: number) {
@@ -12,7 +19,7 @@ export async function getPlayer(playerId: number) {
         return response.data;
     } catch (error) {
         console.error("Error listing player:", error);
-        throw new Error("Unable to list player.");;
+        throw new Error("Unable to list player.");
     }
 }
 
